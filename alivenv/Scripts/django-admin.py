295f943dc7179cfac8ec2eaa9debfc3df374dd2c
Edison Moya Arias <danteejmg@gmail.com>
2017-12-16 18:28:48 -0500

    entorno virtual

---
+++ alivenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dante\desktop\prueba\alan\alive_wp\alivenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
